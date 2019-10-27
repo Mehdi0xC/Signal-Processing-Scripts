e88a556230390eaf3a6011da130a82f17e7cd16b
Mehdi <Mehdi0xC@gmail.com>
2019-10-27 21:19:37 +0330

    adding files

---
+++ SignalGenerator.m
@@ -0,0 +1,18 @@
+% Serial Signal Plotter
+% Developed by Mehdi0xC, April 2018
+MU = [1 2;-3 -5];
+SIGMA = cat(3,[2 0;0 .5],[1 0;0 1]);
+p = ones(1,2)/2;
+obj = gmdistribution(MU,SIGMA,p);
+
+for i=1:10
+    n=64*2.^(i-1);
+    t = 1:1:n;
+    y = random(obj,n);
+    fid=fopen('/Users/mehdi/Desktop/X/signals/signal'+ string(i)+".txt",'w');
+        for k=1:n
+        fprintf( fid , string(y(k,1)) );
+        fprintf( fid ,'\n');
+        end
+    fclose(fid);
+end
