4f9ab9ed990135887134fb92e2f764a67ebb9ccf
Mehdi <Mehdi0xC@gmail.com>
2019-12-07 20:42:24 +0330

    Update

---
+++ GaussianDenoising.m
@@ -0,0 +1,62 @@
+% Gaussian Denoising
+% Developed by Mehdi0xC (Based n Mike X Cohen Signal Processing Course)
+
+% create signal
+srate = 1000; % Hz
+time  = 0:1/srate:3;
+n     = length(time);
+p     = 15; % poles for random interpolation
+
+% noise level, measured in standard deviations
+noiseamp = 5; 
+
+% amplitude modulator and noise level
+ampl   = interp1(rand(p,1)*30,linspace(1,p,n));
+noise  = noiseamp * randn(size(time));
+signal = ampl + noise;
+
+
+% full-width half-maximum: the key Gaussian parameter
+fwhm = 25; % in ms
+
+% normalized time vector in ms
+k = 100;
+gtime = 1000*(-k:k)/srate;
+
+% create Gaussian window
+gauswin = exp( -(4*log(2)*gtime.^2) / fwhm^2 );
+
+% compute empirical FWHM
+pstPeakHalf = k+dsearchn(gauswin(k+1:end)',.5);
+prePeakHalf = dsearchn(gauswin(1:k)',.5);
+
+empFWHM = gtime(pstPeakHalf) - gtime(prePeakHalf);
+
+% show the Gaussian
+figure(1), clf, hold on
+plot(gtime,gauswin,'ko-','markerfacecolor','w','linew',2)
+plot(gtime([prePeakHalf pstPeakHalf]),gauswin([prePeakHalf pstPeakHalf]),'m','linew',3)
+
+% then normalize Gaussian to unit energy
+gauswin = gauswin / sum(gauswin);
+title([ 'Gaussian kernel with requeted FWHM ' num2str(fwhm) ' ms (' num2str(empFWHM) ' ms achieved)' ])
+xlabel('Time (ms)'), ylabel('Gain')
+
+
+% initialize filtered signal vector
+filtsigG = signal;
+
+% implement the running mean filter
+for i=k+1:n-k-1
+    % each point is the weighted average of k surrounding points
+    filtsigG(i) = sum( signal(i-k:i+k).*gauswin );
+end
+
+% plot
+figure(2), clf, hold on
+plot(time,signal,'r')
+plot(time,filtsigG,'k','linew',3)
+
+xlabel('Time (s)'), ylabel('amp. (a.u.)')
+legend({'Original signal';'Gaussian-filtered'})
+title('Gaussian smoothing filter')
--- SignalGenerator.m
+++ SignalGenerator.m
@@ -1,5 +1,5 @@
-% Serial Signal Plotter
-% Developed by Mehdi0xC, April 2018
+% Signal Generator
+% Developed by Mehdi0xC
 MU = [1 2;-3 -5];
 SIGMA = cat(3,[2 0;0 .5],[1 0;0 1]);
 p = ones(1,2)/2;
